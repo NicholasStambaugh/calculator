104e006d799e49b33197632f4d0179bd974e7fed
NicholasStambaugh <nastambaugh@gmail.com>
2023-03-31 11:27:41 -0400

    Adding setup, not tested

---
+++ setup.py
@@ -0,0 +1,19 @@
+import sys
+from cx-Freeze import setup, Executable
+
+# Dependencies are automatically detected, but it might need fine tuning.
+build_exe_options = {"packages": ["tkinter"]}
+
+# GUI applications require a different base on Windows (the default is for a console application).
+base = None
+if sys.platform == "win32":
+    base = "Win32GUI"
+
+# Create the executable.
+setup(
+    name="Calculator App",
+    version="1.0",
+    description="A simple calculator app",
+    options={"build_exe": build_exe_options},
+    executables=[Executable("calculator.py", base=base)]
+)
